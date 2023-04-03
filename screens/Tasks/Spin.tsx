5f5c7c9bbcf87013bde38ce9c9b19218ba1ced3f
Abinash Karmakar <codeAbinash@gmail.com>
2023-04-03 06:03:55 +0000

    Trying navigation bar color change

--- screens/Tasks/Spin.tsx
+++ screens/Tasks/Spin.tsx
@@ -1,37 +1,42 @@
 import { Dimensions, Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import images from '../../assets/images/images'
 import { fonts } from '../../styles/fonts'
+import ButtonFull from '../../components/ButtonFull'
+import { colors } from '../../styles/colors'
+import changeNavigationBarColor, {hideNavigationBar} from 'react-native-navigation-bar-color';
+
 
 export default function Spin() {
   const spinArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   const { width, height } = Dimensions.get('window')
+
+
+  useEffect(() => {
+    // setTimeout(async () => {
+    //   try {
+    //     await changeNavigationBarColor('#012759', true);
+    //   } catch (e) {
+    //     console.log(e)
+    //   }
+    // }, 0);
+    hideNavigationBar();
+  }, [])
   return (
     <View style={{
-      flex: 1, backgroundColor: '#5a0b42', alignItems: 'center', justifyContent: 'space-between'
+      flex: 1, backgroundColor: '#012759', alignItems: 'center', justifyContent: 'space-between'
     }}>
-      <StatusBar backgroundColor="#5a0b42" barStyle="light-content" />
+      <StatusBar backgroundColor="#012759" barStyle="light-content" />
       <View>
-        <Text style={{ fontSize: 25, fontFamily: fonts.semiBold, textAlign: 'center', marginTop: 20, color: 'white', opacity: 0.8 }}>Spin and Earn</Text>
+        <Text style={{ fontSize: 25, fontFamily: fonts.semiBold, textAlign: 'center', marginTop: 20, color: colors.text, opacity: 0.8 }}>Spin and Earn</Text>
       </View>
       <View>
         <Image source={images.spinWheel} style={{
           width: width * (4 / 5) - 20, height: width * (4 / 5) - 20, borderRadius: (width * (4 / 5) - 20) / 2, borderWidth: 2, borderColor: 'white'
         }} />
       </View>
-      <View>
-        <TouchableOpacity activeOpacity={0.8}>
-          <View style={{
-            backgroundColor: '#9c1e47', width: width / 2, borderRadius: 15
-          }}>
-            <Text style={{
-              color: 'white', fontSize: 16, fontFamily: fonts.medium, textAlign: 'center', padding: 20
-            }}>Spin</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View>
-
+      <View style={{ padding: 20, width: width }}>
+        <ButtonFull title="Spin and Win" onPress={() => { }} />
       </View>
     </View >
   )
