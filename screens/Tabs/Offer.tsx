c09e68a6101be3e2390d7fd4b80354328aadff6c
Abinash Karmakar <codeAbinash@gmail.com>
2023-04-09 01:22:44 +0530

    Updated offers screen

--- screens/Tabs/Offer.tsx
+++ screens/Tabs/Offer.tsx
@@ -29,54 +29,46 @@ const Offer = ({ navigation }: any) => {
 
       <View style={{ marginTop: 30 }}>
         <Text style={{ fontSize: 18, fontFamily: fonts.semiBold, textAlign: 'center', color: colors.text }}>Offers to Claim</Text>
-        <View style={{
-          padding: 20,
-        }}>
-          <View style={{
-            // display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            gap: 15,
-            backgroundColor: colors.accent,
-            padding: 15, borderRadius: 20,
-          }}>
-            <View>
-              <Image source={icons.offer} style={{
-                width: 25, height: 25, resizeMode: 'contain',
-                tintColor: "white"
-              }}></Image>
-            </View>
-            <View>
-              <Text style={{
-                fontSize: 16,
-                fontFamily: fonts.regular,
-                color: 'white', marginRight: 35
-              }}>
-                Claim 1000  Coins after completing 10 YouTube valid tasks continuously without any one task gap.
-              </Text>
-
-              <TouchableOpacity>
-
-                <View style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'flex-start',
-                  gap: 10, marginTop: 10
-                }}>
-                  <Image source={icons.youtube_icon} style={{
-                    width: 20,
-                    aspectRatio: 1,
-                    resizeMode: 'contain',
-                  }} />
-                  <Text style={{
-                    fontSize: 15, fontFamily: fonts.semiBold, color: 'white'
-                  }}>Demo Video</Text>
+        <View style={{ padding: 20, gap : 10 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: 15, backgroundColor: colors.accent, padding: 15, borderRadius: 20, }}>
+            <View><Image source={icons.offer} style={{ width: 25, height: 25, resizeMode: 'contain', tintColor: "white" }}></Image></View>
+            <View><Text style={{ fontSize: 16, fontFamily: fonts.regular, color: 'white', marginRight: 35 }}>
+              Claim 1000  Coins after completing 10 YouTube valid tasks continuously without any one task gap.
+            </Text>
+              {/* <TouchableOpacity>
+                <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', gap: 10, marginTop: 10 }}>
+                  <Image source={icons.youtube_icon} style={{ width: 20, aspectRatio: 1, resizeMode: 'contain', }} />
+                  <Text style={{ fontSize: 15, fontFamily: fonts.semiBold, color: 'white' }}>Demo Video</Text>
                 </View>
-              </TouchableOpacity>
+              </TouchableOpacity> */}
             </View>
           </View>
+
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: 15, backgroundColor: colors.accent, padding: 15, borderRadius: 20, }}>
+            <View><Image source={icons.offer} style={{ width: 25, height: 25, resizeMode: 'contain', tintColor: "white" }}></Image></View>
+            <View><Text style={{ fontSize: 16, fontFamily: fonts.regular, color: 'white', marginRight: 35 }}>
+              Claim 1000  Coins after completing 10 YouTube valid tasks continuously without any one task gap.
+            </Text>
+            </View>
+          </View>
+
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: 15, backgroundColor: colors.accent, padding: 15, borderRadius: 20, }}>
+            <View><Image source={icons.offer} style={{ width: 25, height: 25, resizeMode: 'contain', tintColor: "white" }}></Image></View>
+            <View><Text style={{ fontSize: 16, fontFamily: fonts.regular, color: 'white', marginRight: 35 }}>
+              Join a Telegram Channel to claim 100 coins.
+            </Text>
+            </View>
+          </View>
+
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: 15, backgroundColor: colors.accent, padding: 15, borderRadius: 20, }}>
+            <View><Image source={icons.offer} style={{ width: 25, height: 25, resizeMode: 'contain', tintColor: "white" }}></Image></View>
+            <View><Text style={{ fontSize: 16, fontFamily: fonts.regular, color: 'white', marginRight: 35 }}>
+              {/* Install task ( 200 coins ) YouTube png ( clickable ) ( demo video yt link  )  claim button ( link redirect ) Text submit button . */}
+              Install an app and complete a task to claim 200 coins.
+            </Text>
+            </View>
+          </View>
+
         </View>
 
         <Text style={{ fontSize: 18, fontFamily: fonts.semiBold, textAlign: 'center', color: colors.text }}>Completed Tasks</Text>
